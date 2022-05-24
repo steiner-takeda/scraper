a39ccaf78a04cd8c82378feeb4adcef88b5fc5aa
steiner-takeda <peter.steiner@takeda.com>
2022-05-24 21:38:26 +0000

    removed sparktest

--- srilanka.py
+++ srilanka.py
@@ -2,6 +2,11 @@ import urllib.request
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
+import boto3
+import os
+
+aws_access_key_id = os.getenv('AWS_SERVER_PUBLIC_KEY')
+aws_secret_access_key = os.getenv('AWS_SERVER_SECRET_KEY')
 
 today = datetime.today().strftime('%Y-%m-%d')
 
@@ -21,3 +26,12 @@ table = soup.find("table", attrs={"class":"viewDeseases"})
 df = pd.read_html(str(table))[0]
 df = df.dropna()
 df.to_csv("scraper/srilanka/srilanka_cases_by_region_"+today+".csv", index = False)
+
+s3 = boto3.resource(
+    's3', 
+    aws_access_key_id=aws_access_key_id, 
+    aws_secret_access_key=aws_secret_access_key)
+
+s3.Bucket('tak-insight-priv-dengue-gold').upload_file(
+    'malaysia_daily_cases_2022-05-20.csv', 
+    'raw/dengue_cases/malaysia/malaysia_daily_cases_2022-05-21.csv')
