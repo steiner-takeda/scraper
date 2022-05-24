7b0813719b2aaca61da7ae5772dba6feaa9e26bb
steiner-takeda <peter.steiner@takeda.com>
2022-05-24 21:54:05 +0000

    changed

--- malaysia.py
+++ malaysia.py
@@ -13,4 +13,4 @@ soup = BeautifulSoup(html, features="lxml")
 table = soup.find("table", attrs={"class":"table table-bordered"})
 df = pd.read_html(str(table))[0]
 df = df.dropna()
-df.to_csv("scraper/malaysia/malaysia_daily_cases_"+today+".csv", index=False)
+df.to_csv("scraper/malaysia/malaysia_daily_cases_"+today+".csv", index = False)
